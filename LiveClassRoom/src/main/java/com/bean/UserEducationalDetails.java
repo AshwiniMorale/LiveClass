9a4c4750b75895b2fc1fe18cd2b454edd3d71f9e
cyberdroid <cyberdroid@CyberDroid-PC>
2019-04-22 11:24:08 +0530

    update ishita code

--- LiveClassRoom/src/main/java/com/bean/UserEducationalDetails.java
+++ LiveClassRoom/src/main/java/com/bean/UserEducationalDetails.java
@@ -7,14 +7,20 @@ import javax.persistence.*;
 public class UserEducationalDetails {
 
 	@Id
-	 @GeneratedValue
-	 private int userId;
+	@GeneratedValue
+	private int educationId;
 	@Column
-	private int Xth;
+	private int userId;
+	@Column
+	private int XthPercentage;
+	@Column
+	private int XthYear;
 	@Column
 	private String XthBoard;
 	@Column
-	private int XIIth;
+	private int XIIthPercentage;
+	@Column
+	private int XIIthYear;
 	@Column
 	private String XIIthBoard;
 	@Column
@@ -25,57 +31,85 @@ public class UserEducationalDetails {
 	private int UGMarks;
 	@Column
 	private String University;
+
+	public int getEducationId() {
+		return educationId;
+	}
+
+	public void setEducationId(int educationId) {
+		this.educationId = educationId;
+	}
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "userId")
 	public int getUserId() {
 		return userId;
 	}
+
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
+
 	public int getXth() {
-		return Xth;
+		return XthPercentage;
 	}
-	public void setXth(int xth) {
-		Xth = xth;
+
+	public void setXth(int xthPercentage) {
+		XthPercentage = xthPercentage;
 	}
+
 	public String getXthBoard() {
 		return XthBoard;
 	}
+
 	public void setXthBoard(String xthBoard) {
 		XthBoard = xthBoard;
 	}
+
 	public int getXIIth() {
-		return XIIth;
+		return XIIthPercentage;
 	}
-	public void setXIIth(int xIIth) {
-		XIIth = xIIth;
+
+	public void setXIIth(int xIIthPercentage) {
+		XIIthPercentage = xIIthPercentage;
 	}
+
 	public String getXIIthBoard() {
 		return XIIthBoard;
 	}
+
 	public void setXIIthBoard(String xIIthBoard) {
 		XIIthBoard = xIIthBoard;
 	}
+
 	public String getUG() {
 		return UG;
 	}
+
 	public void setUG(String uG) {
 		UG = uG;
 	}
+
 	public String getStream() {
 		return Stream;
 	}
+
 	public void setStream(String stream) {
 		Stream = stream;
 	}
+
 	public int getUGMarks() {
 		return UGMarks;
 	}
+
 	public void setUGMarks(int uGMarks) {
 		UGMarks = uGMarks;
 	}
+
 	public String getUniversity() {
 		return University;
 	}
+
 	public void setUniversity(String university) {
 		University = university;
 	}
